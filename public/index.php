18e93d203e94bfe8f3d6e50aec7b5d73765dd3ef
Elias <politico@mail.ru>
2021-12-08 19:59:43 +0300

    monolog added

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo phpinfo(INFO_GENERAL); 
+require_once('../vendor/autoload.php');
+
+echo phpinfo(); 
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('log/my.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+
+$log->warning('Memory 1' . memory_get_usage());
+
+echo phpinfo();
+
+$log->warning('Memory 2' . memory_get_usage());
+$log->warning(print_r($log, true));
